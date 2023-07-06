d2ffa3d17f029d02cb4d6672918a4505bd07375a
Indigowar <indigowar.github@gmail.com>
2023-07-07 01:06:54 +0300

    added the interfaces for handlers

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,67 @@
 package main
 
-import "net/http"
+import (
+	"bookstore/internal/delivery/http/handlers"
+	"bookstore/internal/services"
+	inmem "bookstore/internal/storages/in_memory/book"
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
+	"golang.org/x/exp/slog"
+)
 
 func main() {
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		writer.Write([]byte("hello, world"))
+	logger := slog.Default()
+
+	storage := inmem.NewInMemoryBookStorage()
+
+	bookSvc := services.NewBookService(logger, storage)
+
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(render.SetContentType(render.ContentTypeJSON))
+
+	r.Route("/books", func(r chi.Router) {
+		r.Get("/", handlers.MakeGetAllBooksHandler(bookSvc))
+		r.Get("/{id}", handlers.MakeGetSpecificBookHandler(bookSvc))
+		r.Post("/", handlers.MakeCreateBookHandler(bookSvc))
+		r.Delete("/{id}", handlers.MakeDeleteBookHandler(bookSvc))
+		r.Put("/", handlers.MakeUpdateBookHandler(bookSvc))
 	})
 
-	http.ListenAndServe(":8000", nil)
+	server := &http.Server{
+		Addr:    ":8000",
+		Handler: r,
+	}
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			logger.Error(err.Error())
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+
+	<-quit
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		logger.Error(err.Error())
+		log.Fatal("Failed to stop the server. Forced to shut it down.")
+	}
+
+	log.Println("Server is shot down.")
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module bookstore
 go 1.20
 
 require (
+	github.com/ajg/form v1.5.1 // indirect
 	github.com/go-chi/chi/v5 v5.0.8 // indirect
+	github.com/go-chi/render v1.0.2 // indirect
 	github.com/jmoiron/sqlx v1.3.5 // indirect
 	golang.org/x/exp v0.0.0-20230626212559-97b1e661b5df // indirect
 )
